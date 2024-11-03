09428bbc0626f2fdbd2b3c04ed4b186e230de1e3
JosueAguilar2024 <erojasa@miumg.edu.gt>
2024-11-02 19:34:25 -0600

    Seguimos viendo no hemos cambiando nada

--- src/main/java/com/mycompany/proyectofinalmultplay/GestionarArchivos.java
+++ src/main/java/com/mycompany/proyectofinalmultplay/GestionarArchivos.java
@@ -3,7 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.proyectofinalmultplay;
+import com.mpatric.mp3agic.ID3v1;
+import com.mpatric.mp3agic.ID3v2;
+import com.mpatric.mp3agic.Mp3File;
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import javax.swing.JFileChooser;
 import javax.swing.table.DefaultTableModel;
@@ -15,73 +19,56 @@ import javax.swing.JOptionPane;
 public class GestionarArchivos {
 
     private DefaultTableModel modeloTabla;
-
+    private File carpetaSeleccionada; // Almacena la carpeta seleccionada
+   
+    
     public GestionarArchivos(DefaultTableModel modeloTabla) {
         this.modeloTabla = modeloTabla;
+      
     }
 
-    // Método para listar archivos en un directorio seleccionado por el usuario
-    public void listarArchivos() {
+    // obtenerla para pder usarla
+    public File getCarpetaSeleccionada() {
+        return carpetaSeleccionada;
+    }
+
+    // Método para seleccionar la carpeta que el usuario desea explorar
+    public void seleccionarCarpeta() {
         JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY); // carpetas
 
         int returnValue = fileChooser.showOpenDialog(null);
         if (returnValue == JFileChooser.APPROVE_OPTION) {
-            File carpeta = fileChooser.getSelectedFile();
-            listarArchivosEnCarpeta(carpeta);
-        }
-    }
-
-    // Método para listar archivos en la carpeta seleccionada
-    public void listarArchivosEnCarpeta(File carpeta) {
-        modeloTabla.setRowCount(0); // Limpiar la tabla antes de listar
-
-        File[] archivos = carpeta.listFiles();
-        if (archivos != null) {
-            for (File archivo : archivos) {
-                if (archivo.isFile()) {
-                    String[] datos = new String[7];
-                    datos[0] = archivo.getName(); // NOMBRE
-                    datos[1] = archivo.getName(); // ARCHIVO (puedes cambiarlo si es necesario)
-                    datos[2] = ""; // AUTOR (debes implementarlo)
-                    datos[3] = ""; // ALBUM (debes implementarlo)
-                    datos[4] = ""; // GENERO (debes implementarlo)
-                    datos[5] = String.valueOf(archivo.length()); // TAMAÑO
-                    datos[6] = archivo.getAbsolutePath(); // RUTA
-                    modeloTabla.addRow(datos);
-                } else if (archivo.isDirectory()) {
-                // Si el archivo es un directorio, llamar recursivamente a la misma función
-             listarArchivosEnCarpeta(archivo);
-            }
-            }
+            carpetaSeleccionada = fileChooser.getSelectedFile();
         } else {
-            JOptionPane.showMessageDialog(null, "No se encontraron archivos en la carpeta seleccionada.");
-        }
-    }
-    // Método para filtrar archivos por tipo (musica, videos, fotos)
-    public void filtrarArchivos(String tipo) {
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-
-        int returnValue = fileChooser.showOpenDialog(null);
-        if (returnValue == JFileChooser.APPROVE_OPTION) {
-            File carpeta = fileChooser.getSelectedFile();
-            filtrarArchivosEnCarpeta(carpeta, tipo);
+            JOptionPane.showMessageDialog(null, "No se seleccionó ninguna carpeta.");
         }
     }
 
-    // Método para filtrar archivos en la carpeta seleccionada
+    
+    public void filtrarArchivosPorTipo(String tipo) {
+        if (carpetaSeleccionada == null) {
+            return;
+        }
+          modeloTabla.setRowCount(0);
+       
+        filtrarArchivosEnCarpeta(carpetaSeleccionada, tipo);
+    }
+
+    // Método recursivo para listar y filtrar archivos en la carpeta y sus subcarpetas
     public void filtrarArchivosEnCarpeta(File carpeta, String tipo) {
-        modeloTabla.setRowCount(0); // Limpiar la tabla antes de listar
-
-        File[] archivos = carpeta.listFiles();
+        File[] archivos = carpeta.listFiles(); // Lista de archivos y carpetas en el directorio actual
         if (archivos != null) {
             for (File archivo : archivos) {
-                if (archivo.isFile()) {
+                if (archivo.isDirectory()) {
+                    // Si es un directorio, llamada recursiva para explorar su contenido
+                    filtrarArchivosEnCarpeta(archivo, tipo);
+                } else if (archivo.isFile()) {
+                    // Si es un archivo, verifica su tipo (extensión) para decidir si se agrega a la tabla
                     String extension = getFileExtension(archivo).toLowerCase();
                     boolean agregar = false;
 
-                    switch (tipo.toLowerCase()) {
+                    switch (tipo.toLowerCase()) {//con minuscula
                         case "musica":
                             if (extension.equals("mp3") || extension.equals("wav") || extension.equals("flac")) {
                                 agregar = true;
@@ -98,20 +85,33 @@ public class GestionarArchivos {
                             }
                             break;
                         case "todos":
-                            agregar = true; // Agregar todos los archivos
+                            agregar = true; // Agrega todos los archivos independientemente de la extensión
                             break;
                     }
 
                     if (agregar) {
+                        // Datos para agregar el archivo a la tabla
                         String[] datos = new String[7];
                         datos[0] = archivo.getName(); // NOMBRE
-                        datos[1] = archivo.getName(); // ARCHIVO (puedes cambiarlo si es necesario)
-                        datos[2] = ""; // AUTOR (debes implementarlo)
-                        datos[3] = ""; // ALBUM (debes implementarlo)
-                        datos[4] = ""; // GENERO (debes implementarlo)
-                        datos[5] = String.valueOf(archivo.length()); // TAMAÑO
+                        datos[1] = archivo.getName(); // ARCHIVO
+                        datos[2] = obtenerAutor(archivo);// AUTOR 
+                        datos[3] = obtenerAlbum(archivo);  // ALBUM
+                        datos[4] = obtenerGenero(archivo);
+                      
+                       long tamañoEnBytes = archivo.length();
+                    String tamañoFormato;
+         
+                    if (tamañoEnBytes >= 1024 * 1024 * 1024) { 
+                        double tamañoEnGB = tamañoEnBytes / (1024.0 * 1024.0 * 1024.0);
+                        tamañoFormato = String.format("%.2f GB", tamañoEnGB); // Formato en GB
+                    } else {
+                        double tamañoEnMB = tamañoEnBytes / (1024.0 * 1024.0);
+                        tamañoFormato = String.format("%.2f MB", tamañoEnMB); // Formato en MB
+                    }
+                        datos[5] = tamañoFormato; 
                         datos[6] = archivo.getAbsolutePath(); // RUTA
-                        modeloTabla.addRow(datos);
+
+                        modeloTabla.addRow(datos); // Agrega el archivo a la tabla
                     }
                 }
             }
@@ -125,8 +125,61 @@ public class GestionarArchivos {
         String nombreArchivo = file.getName();
         int lastIndex = nombreArchivo.lastIndexOf('.');
         if (lastIndex > 0 && lastIndex < nombreArchivo.length() - 1) {
-            return nombreArchivo.substring(lastIndex + 1);
+            return nombreArchivo.substring(lastIndex + 1); // Retorna la extensión del archivo
         }
-        return "";
+        return ""; // Retorna una cadena vacía si no tiene extensión
     }
+    public String obtenerAutor(File archivo) {
+          try {
+        if (archivo.getName().endsWith(".mp3")) {
+            Mp3File mp3File = new Mp3File(archivo);
+            if (mp3File.hasId3v2Tag()) {
+                ID3v2 id3v2Tag = mp3File.getId3v2Tag();
+                return id3v2Tag.getArtist() != null ? id3v2Tag.getArtist() : "Autor desconocido";
+            } else if (mp3File.hasId3v1Tag()) {
+                ID3v1 id3v1Tag = mp3File.getId3v1Tag();
+                return id3v1Tag.getArtist() != null ? id3v1Tag.getArtist() : "Autor desconocido";
+            }
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    return "Autor desconocido";
+}
+    
+    public String obtenerAlbum(File archivo) {
+       try {
+        if (archivo.getName().endsWith(".mp3")) {
+            Mp3File mp3File = new Mp3File(archivo);
+            if (mp3File.hasId3v2Tag()) {
+                ID3v2 id3v2Tag = mp3File.getId3v2Tag();
+                return id3v2Tag.getAlbum() != null ? id3v2Tag.getAlbum() : "Álbum desconocido";
+            } else if (mp3File.hasId3v1Tag()) {
+                ID3v1 id3v1Tag = mp3File.getId3v1Tag();
+                return id3v1Tag.getAlbum() != null ? id3v1Tag.getAlbum() : "Álbum desconocido";
+            }
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    return "Álbum desconocido";
+    }
+    public String obtenerGenero(File archivo) {
+    try {
+        if (archivo.getName().endsWith(".mp3")) {
+            Mp3File mp3File = new Mp3File(archivo);
+            if (mp3File.hasId3v2Tag()) {
+                ID3v2 id3v2Tag = mp3File.getId3v2Tag();
+                return id3v2Tag.getGenreDescription() != null ? id3v2Tag.getGenreDescription() : "Género desconocido";
+            } else if (mp3File.hasId3v1Tag()) {
+                ID3v1 id3v1Tag = mp3File.getId3v1Tag();
+                return id3v1Tag.getGenre() != -1 ? id3v1Tag.getGenreDescription() : "Género desconocido";
+            }
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    return "Género desconocido";
+}
+
 }
